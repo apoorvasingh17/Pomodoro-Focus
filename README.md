f8a543875c81ed83518a22d1d4ec5b90e7fe51c0
apoorvasingh17 <apoorvasingh2811@gmail.com>
2017-12-29 19:03:05 +0530

    added content script

--- README.md
+++
@@ -1,2 +0,0 @@
-# Pomodoro-Focus
-A web-extension that enables users to block particular websites that distract them for a certain duration of time, to be able to focus on their work. This idea is inspired by the Pomodoro technique. 
--- options.js
+++ options.js
@@ -1,4 +1,27 @@
-$("#blockthistoo").click(function () {
-    PF.addBlockedSite($("#dontgothere").val());
-    ...
-});
+document.addEventListener('DOMContentLoaded',
+  function () {
+    // Initialize things
+    if (localStorage["enabled"] !== "false") {
+      document.getElementById('enabled').checked = true;
+    } else {
+      var checked = !document.getElementById('enabled').checked;
+      document.getElementById('blacklist').disabled = checked;
+    }
+
+    var bl = localStorage["blacklist"];
+    document.getElementById('blacklist').value = bl ? bl : "";
+
+    // And attach signals
+    document.getElementById('blacklist').addEventListener('keyup',
+      function () {
+        var blacklist = document.getElementById('blacklist').value;
+        localStorage["blacklist"] = blacklist;
+      });
+
+    document.getElementById('enabled').addEventListener('click',
+      function() {
+        var checked = document.getElementById('enabled').checked;
+        document.getElementById('blacklist').disabled = !checked;
+        localStorage["enabled"] = checked;
+      });
+  });
