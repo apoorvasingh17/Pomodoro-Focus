8b94876fe42a34b4b4e0871d21f26302e3376085
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2017-12-31 00:36:58 +0530

    adding timer etc.

---
+++ bg.js
@@ -0,0 +1,34 @@
+console.log("bgscript init");
+var blockState = false;
+var expiryTime = 30000;//block sites for 10 seconds
+var timeleft = 0;
+function startTimer() {
+    console.log('timerStart');
+    blockState = true;
+    timeleft = expiryTime/1000;
+    var timerDeduct = setInterval(function () {
+        timeleft--;
+    }, 1000);
+    setTimeout(function () {
+        blockState = false;
+        console.log('timerExp');
+        clearInterval(timeleft);
+    }, expiryTime);
+}
+function forceEndSession() {
+    blockState = false;  
+    clearInterval(timeleft);
+}
+function getTimeLeft(){
+    return timeleft;
+}
+chrome.webRequest.onBeforeRequest.addListener(
+    function () {
+        console.log('called',blockState);
+        return {
+            cancel: (blockState)
+        };
+    }, {
+        urls: ["*://*.facebook.com/*", "*://*.youtube.com/*",  "*://*.instagram.com/*"]//edit here to load urls from localstorage as an array of strings like "*://*.facebook.*/*" also refresh background script whenever list is updated
+    }, ["blocking"]
+);
--- options.js
+++ options.js
@@ -1,5 +1,37 @@
+function writeTimeLeft(val) {
+  document.getElementById('timeLeft').innerText = val;
+}
+var myTime = chrome.extension.getBackgroundPage().getTimeLeft();
+console.log(myTime);
+var decTimer = setInterval(function () {
+  checkTimeout();
+  document.getElementById('timeLeft').innerText = myTime +" seconds left.";
+  if(myTime==0){
+    clearInterval(decTimer);
+    return;
+  }
+  myTime--;
+}, 1000);
+function checkTimeout() {
+  if(myTime==0){
+    document.getElementById("startTimer").style.display = 'block';
+    document.getElementById("timeLeft").style.display = 'none';
+  }
+  else if (myTime != 0){
+    document.getElementById("startTimer").style.display = 'none';
+    document.getElementById("timeLeft").style.display = 'block';
+  }
+}
 document.addEventListener('DOMContentLoaded',
   function () {
+    // console.log(window.location);
+    document.getElementById('startTimer').addEventListener("click", function(){//hide this button when timer has started 
+      chrome.extension.getBackgroundPage().startTimer();
+      myTime = chrome.extension.getBackgroundPage().getTimeLeft();
+      checkTimeout();
+    });
+
+    //start timer here to show it deducting every second; init time can be fetched from  chrome.extension.getBackgroundPage().getTimeLeft()
     // Initialize things
     if (localStorage["enabled"] !== "false") {
       document.getElementById('enabled').checked = true;
