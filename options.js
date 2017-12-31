999fb105b74555d0e905c012f6e41d81d15427a9
apoorvasingh17 <apoorvasingh2811@gmail.com>
2017-12-31 14:10:28 +0530

    edited the background script, added a timer

--- options.js
+++ options.js
@@ -1,6 +1,51 @@
+function writeTimeLeft(val) {
+  document.getElementById('timeLeft').innerText = val;
+}
+
+var myTime = chrome.extension.getBackgroundPage().getTimeLeft();
+console.log(myTime);
+var decTimer = setInterval(function () {
+  checkTimeout();
+  document.getElementById('timeLeft').innerText = myTime +" seconds left.";
+  if(myTime==0){
+    clearInterval(decTimer);
+    return;
+  }
+  myTime--;
+}, 1000);
+function checkTimeout() {
+  if(myTime==0){
+    document.getElementById("startTimer").style.display = 'block';
+    document.getElementById("timeLeft").style.display = 'none';
+
+    var blacklist = document.getElementById('blacklist').value;
+    document.getElementById('blacklist').disabled = false;
+    localStorage["blacklist"] = blacklist;
+  }
+  else if (myTime != 0){
+    document.getElementById("startTimer").style.display = 'none';
+    document.getElementById("timeLeft").style.display = 'block';
+    var blacklist = document.getElementById('blacklist').value;
+    document.getElementById('blacklist').disabled = true;
+    localStorage["blacklist"] = blacklist;
+
+  }
+}
 document.addEventListener('DOMContentLoaded',
   function () {
-    // Initialize things
+    
+    document.getElementById('startTimer').addEventListener("click", function(){
+
+       var blacklist = document.getElementById('blacklist').value;
+       document.getElementById('blacklist').disabled = true;
+       localStorage["blacklist"] = blacklist;
+
+      chrome.extension.getBackgroundPage().startTimer();
+      myTime = chrome.extension.getBackgroundPage().getTimeLeft();
+      checkTimeout();
+    });
+
+  
     if (localStorage["enabled"] !== "false") {
       document.getElementById('enabled').checked = true;
     } else {
@@ -11,7 +56,7 @@ document.addEventListener('DOMContentLoaded',
     var bl = localStorage["blacklist"];
     document.getElementById('blacklist').value = bl ? bl : "";
 
-    // And attach signals
+    
     document.getElementById('blacklist').addEventListener('keyup',
       function () {
         var blacklist = document.getElementById('blacklist').value;
